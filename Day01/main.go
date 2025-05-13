0dc1e49823b25ef81e0d5e4f60ce13c514eca7fd
Jason Waters <jasonw@plumbersstock.com>
2025-05-13 08:14:35 -0600

    Added day01: I missed it aparently

---
+++ Day01/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+	fmt.Println("My name is Jason, and I'm here to learn.")
+
+	var age int = 47
+
+	fmt.Printf("I am %d years old. \n", age)
+
+	language := "Golang"
+	day := "day01"
+	fmt.Printf("I'm learning %s this is %s", language, day)
+}
